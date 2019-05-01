693716b23756968a614692cba5a8b6e03a286388
Xavier Vila <xvila@squarespace.com>
2019-05-01 13:07:51 -0400

    Chapter 3

---
+++ app/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    # Validators are things that validate the field
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In') # these are labels for the fields
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 # logic of the application
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
+from app.forms import LoginForm
 
 @app.route('/index')# you can chain routes
 @app.route('/') # associate the top level url with this function
@@ -17,4 +18,15 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts) 
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data
+        ))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
 
