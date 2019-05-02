5ac89bada15bb598adfd1c3e2d2cfd24e5316ebf
Xavier Vila <xvila@squarespace.com>
2019-05-02 16:28:07 -0400

    chapter 4

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 # logic of the application
 from app import app
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 
 @app.route('/index')# you can chain routes
@@ -20,13 +20,14 @@ def index():
     return render_template('index.html', title='Home', user=user, posts=posts) 
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data
-        ))
-        return redirect('/index')
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
 
+# url_for builds a url for a specific function. So rather than manually typing in a url such as '/index' you can type 
+# url_for(index) to generate the links. index is the name of the function such as def index():
